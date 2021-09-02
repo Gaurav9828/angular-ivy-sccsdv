95b6bb973c8676f3b195f09893c28805c8f5e77e
Gaurav9828 <gauravsri9828@gmail.com>
2021-09-02 16:55:11 +0530

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,9 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { LandingPageComponent } from './landing-page/landing-page.component';
-import { LoginComponent } from './landing-page/login/login.component';
 @NgModule({
   imports: [BrowserModule, FormsModule],
-  declarations: [LandingPageComponent, LoginComponent],
+  declarations: [LandingPageComponent],
   bootstrap: [LandingPageComponent]
 })
 export class AppModule {}
