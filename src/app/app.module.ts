69e0c4899eaa2a58f5f36fafe938da77fcb9eb80
Gaurav9828 <gauravsri9828@gmail.com>
2021-09-02 18:19:07 +0530

    login Page UI completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { LandingPageComponent } from './landing-page/landing-page.component';
+import { AdminloginComponent } from './landing-page/adminlogin/adminlogin.component';
 @NgModule({
   imports: [BrowserModule, FormsModule],
-  declarations: [LandingPageComponent],
+  declarations: [LandingPageComponent, AdminloginComponent],
   bootstrap: [LandingPageComponent]
 })
 export class AppModule {}
